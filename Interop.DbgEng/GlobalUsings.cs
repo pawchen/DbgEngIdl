a8443692b41e5d3584ae8c9ab426f636dc75bf52
Paul Chen <lancevdance@gmail.com>
2025-11-05 11:28:55 +0800

    Add more integer types.

--- Interop.DbgEng/GlobalUsings.cs
+++ Interop.DbgEng/GlobalUsings.cs
@@ -1,9 +1,47 @@
 ﻿// See https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types
 
 global using BYTE = System.Byte;
+
 global using WORD = System.UInt16;
 global using DWORD = System.UInt32;
-global using ULONG = System.UInt32;
+global using DWORD32 = System.UInt32;
+global using DWORD64 = System.UInt64;
+global using QWORD = System.UInt64;
+global using DWORDLONG = System.UInt64;
+
+global using BOOL = System.Int32;
+global using BOOLEAN = System.Byte;
+
+global using CHAR = System.Byte;
+global using CCHAR = System.Byte;
+global using UCHAR = System.Byte;
+
+global using USHORT = System.UInt16;
+global using SHORT = System.Int16;
+
+global using UINT = System.UInt32;
+global using ULONG = System.UInt32; // ULONG = UINT
+
+global using INT = System.Int32;
+global using LONG = System.Int32; // LONG = INT
+
 global using ULONGLONG = System.UInt64;
-global using LONG = System.Int32;
 global using LONGLONG = System.Int64;
+
+global using UINT8 = System.Byte;
+global using UINT16 = System.UInt16;
+global using UINT32 = System.UInt32;
+global using UINT64 = System.UInt64;
+
+global using ULONG32 = System.UInt32;
+global using ULONG64 = System.UInt64;
+
+global using INT8 = System.SByte;
+global using INT16 = System.Int16;
+global using INT32 = System.Int32;
+global using INT64 = System.Int64;
+
+global using LONG32 = System.Int32;
+global using LONG64 = System.Int64;
+
+global using HRESULT = System.Int32;
