3de11206a128a59d41e9537bfe06a8f84d43f814
Paul Chen <lancevdance@gmail.com>
2025-11-14 11:31:39 +0800

    Add missing assert.

--- SrcGen/Program.cs
+++ SrcGen/Program.cs
@@ -674,7 +674,7 @@ namespace SrcGen
                                 }
                                 else if (nativeType.EndsWith("STR"))
                                 {
-                                    Debug.Assert(isIn);
+                                    Debug.Assert(isIn && !isOut);
 
                                     generatedName = "string";
 
